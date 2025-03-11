16dca069dc68254cb14e264fb7193d693e82666a
Nguyen Nhu Tuan <tuan.nn@htigroup.vn>
2025-03-11 14:46:17 +0700

    feat: Add check UI for cron job

--- VNNewsCrawler.py
+++ VNNewsCrawler.py
@@ -3,6 +3,7 @@ import argparse
 from logger import log
 from utils import utils
 from crawler.factory import get_crawler
+from ui_checker import UIChecker
 
 
 def main(config_fpath):
@@ -10,8 +11,18 @@ def main(config_fpath):
     log.setup_logging(log_dir=config["output_dpath"], 
                       config_fpath=config["logger_fpath"])
     webnames = config.get("webnames", [])
+    # Khởi tạo UI Checker
+    ui_checker = UIChecker(config["ui_hash_file"])
+
     for webname in webnames:
         print(f"🚀 Bắt đầu crawl báo: {webname}")
+
+        # Lấy URL của trang báo từ config
+        base_url = config["news_sites"].get(webname, "")
+
+        if ui_checker.check_ui_change(base_url):
+            print(f"UI của {webname} đã thay đổi! Bỏ qua crawling.")
+            continue
 
         # Tạo một bản config mới cho từng báo
         custom_config = config.copy()
