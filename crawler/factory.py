5c1674d17426e0f87a9ce0a4074b0378cbb347ce
Nguyen Nhu Tuan <tuan.nn@htigroup.vn>
2025-04-18 15:04:01 +0700

    feat: Add crawler dan tri

--- crawler/factory.py
+++ crawler/factory.py
@@ -1,7 +1,9 @@
 from .vnexpress import VNExpressCrawler
+from .dantri import DanTriCrawler
 from .vietnamnet import VietNamNetCrawler
 
 WEBNAMES = {"vnexpress": VNExpressCrawler,
+            "dantri": DanTriCrawler,
             "vietnamnet": VietNamNetCrawler}
 
 def get_crawler(webname, **kwargs):
