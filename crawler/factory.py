979422813552ce3908cad2ce0512840ecc00f971
Nguyen Nhu Tuan <tuan.nn@htigroup.vn>
2025-04-23 16:28:33 +0700

    feat: Add crawler for quan doi nhan dan

--- crawler/factory.py
+++ crawler/factory.py
@@ -2,11 +2,13 @@ from .vnexpress import VNExpressCrawler
 from .dantri import DanTriCrawler
 from .vietnamnet import VietNamNetCrawler
 from .tapchitoaan import TapChiToaAnCrawler
+from .quandoinhandan import QuanDoiNhanDanCrawler
 
 WEBNAMES = {"vnexpress": VNExpressCrawler,
             "dantri": DanTriCrawler,
             "vietnamnet": VietNamNetCrawler,
-            "tapchitoaan": TapChiToaAnCrawler}
+            "tapchitoaan": TapChiToaAnCrawler,
+            "quandoinhandan": QuanDoiNhanDanCrawler}
 
 def get_crawler(webname, **kwargs):
     crawler = WEBNAMES[webname](**kwargs)
