935f9b3f57a4408b85ca45e275b0f059c7986947
Nguyen Nhu Tuan <tuan.nn@htigroup.vn>
2025-04-23 08:56:15 +0700

    feat: Add crawler tap chi toa an

--- crawler/factory.py
+++ crawler/factory.py
@@ -1,10 +1,12 @@
 from .vnexpress import VNExpressCrawler
 from .dantri import DanTriCrawler
 from .vietnamnet import VietNamNetCrawler
+from .tapchitoaan import TapChiToaAnCrawler
 
 WEBNAMES = {"vnexpress": VNExpressCrawler,
             "dantri": DanTriCrawler,
-            "vietnamnet": VietNamNetCrawler}
+            "vietnamnet": VietNamNetCrawler,
+            "tapchitoaan": TapChiToaAnCrawler}
 
 def get_crawler(webname, **kwargs):
     crawler = WEBNAMES[webname](**kwargs)
