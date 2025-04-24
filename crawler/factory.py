f4e316b38aee0493c00856867a26573d9d670680
Nguyen Nhu Tuan <tuan.nn@htigroup.vn>
2025-04-24 09:50:33 +0700

    feat: add crawler bao van hoa

--- crawler/factory.py
+++ crawler/factory.py
@@ -3,12 +3,14 @@ from .dantri import DanTriCrawler
 from .vietnamnet import VietNamNetCrawler
 from .tapchitoaan import TapChiToaAnCrawler
 from .quandoinhandan import QuanDoiNhanDanCrawler
+from .baovanhoa import BaoVanHoaCrawler
 
 WEBNAMES = {"vnexpress": VNExpressCrawler,
             "dantri": DanTriCrawler,
             "vietnamnet": VietNamNetCrawler,
             "tapchitoaan": TapChiToaAnCrawler,
-            "quandoinhandan": QuanDoiNhanDanCrawler}
+            "quandoinhandan": QuanDoiNhanDanCrawler,
+            "baovanhoa": BaoVanHoaCrawler}
 
 def get_crawler(webname, **kwargs):
     crawler = WEBNAMES[webname](**kwargs)
