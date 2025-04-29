9ab227567715cfbfd0d6748c7528790e44ffc7d5
lantuan2205 <minhhuy22051999@gmail.com>
2025-04-29 09:00:04 +0000

    feat: Update articles type for news

--- crawler/vnexpress.py
+++ crawler/vnexpress.py
@@ -32,23 +32,77 @@ class VNExpressCrawler(BaseCrawler):
         self.__dict__.update(kwargs)
         self.logger = log.get_logger(name=__name__)
         self.article_type_dict = {
-            0: "thoi-su",
-            1: "the-gioi",
-            2: "kinh-doanh",
-            3: "cong-nghe",
-            4: "khoa-hoc",
-            5: "goc-nhin",
-            6: "bat-dong-san",
-            7: "suc-khoe",
-            8: "the-thao",
-            9: "giai-tri",
-            10: "phap-luat",
-            11: "giao-duc",
-            12: "doi-song",
-            13: "xe",
-            14: "du-lich",
-            15: "y-kien",
-            16: "tam-su",
+            0: "thoi-su/chinh-tri",
+            1: "thoi-su/huong-toi-ky-nguyen-moi/tinh-gon-bo-may",
+            2: "thoi-su/chinh-tri/nhan-su",
+            3: "thoi-su/dan-sinh",
+            4: "thoi-su/lao-dong-viec-lam",
+            5: "thoi-su/giao-thong",
+            6: "thoi-su/mekong",
+            7: "thoi-su/quy-hy-vong",
+            8: "the-gioi/tu-lieu",
+            9: "the-gioi/phan-tich",
+            10: "the-gioi/nguoi-viet-5-chau",
+            11: "the-gioi/cuoc-song-do-day",
+            12: "the-gioi/quan-su",
+            13: "kinh-doanh/net-zero",
+            14: "kinh-doanh/quoc-te",
+            15: "kinh-doanh/doanh-nghiep",
+            16: "kinh-doanh/chung-khoan",
+            17: "kinh-doanh/ebank",
+            18: "kinh-doanh/vi-mo",
+            19: "kinh-doanh/tien-cua-toi",
+            20: "kinh-doanh/hang-hoa",
+            21: "khoa-hoc-cong-nghe/bo-khoa-hoc-va-cong-nghe",
+            22: "khoa-hoc-cong-nghe/chuyen-doi-so",
+            23: "khoa-hoc-cong-nghe/doi-moi-sang-tao",
+            24: "khoa-hoc-cong-nghe/ai",
+            25: "khoa-hoc-cong-nghe/vu-tru",
+            26: "khoa-hoc-cong-nghe/the-gioi-tu-nhien",
+            27: "khoa-hoc-cong-nghe/thiet-bi",
+            28: "khoa-hoc-cong-nghe/cua-so-tri-thuc",
+            29: "bat-dong-san/chinh-sach",
+            30: "bat-dong-san/thi-truong",
+            31: "bat-dong-san/du-an",
+            32: "bat-dong-san/khong-gian-song",
+            33: "suc-khoe/tin-tuc",
+            34: "suc-khoe/cac-benh",
+            35: "suc-khoe/song-khoe",
+            36: "suc-khoe/vaccine",
+            37: "the-thao/marathon",
+            38: "bong-da",
+            39: "the-thao/tennis",
+            40: "the-thao/cac-mon-khac",
+            41: "the-thao/hau-truong",
+            42: "giai-tri/gioi-sao",
+            43: "giai-tri/sach",
+            44: "giai-tri/phim",
+            45: "giai-tri/nhac",
+            46: "giai-tri/thoi-trang",
+            47: "giai-tri/lam-dep",
+            48: "giai-tri/san-khau-my-thuat",
+            49: "phap-luat/ho-so-pha-an",
+            50: "giao-duc/tin-tuc",
+            51: "giao-duc/tuyen-sinh/lop-6",
+            52: "giao-duc/chan-dung",
+            53: "giao-duc/tuyen-sinh/lop-10",
+            54: "giao-duc/tuyen-sinh/dai-hoc",
+            55: "giao-duc/du-hoc",
+            56: "giao-duc/thao-luan",
+            57: "giao-duc/hoc-tieng-anh",
+            58: "giao-duc/giao-duc-40",
+            59: "doi-song/nhip-song",
+            60: "doi-song/to-am",
+            61: "doi-song/bai-hoc-song",
+            62: "doi-song/tieu-dung",
+            63: "oto-xe-may/thi-truong",
+            64: "oto-xe-may/xe-dien",
+            65: "oto-xe-may/cam-lai",
+            66: "du-lich/diem-den",
+            67: "du-lich/am-thuc",
+            68: "du-lich/dau-chan",
+            69: "y-kien/thoi-su",
+            70: "y-kien/doi-song"
         }
 
     def download_image(self, image_url, article_title, category, published_date):
@@ -65,7 +119,7 @@ class VNExpressCrawler(BaseCrawler):
             date_parts = clean_date(published_date).split(',')[0].strip()  # Lấy phần trước dấu phẩy
             day, month, year = date_parts.split('/')  # Tách ngày, tháng, năm
             date_folder = f"{day}-{month}-{year}"  # Tạo định dạng mới
-            
+
             # Tạo đường dẫn thư mục đầy đủ
             remote_dir = Path(remote_base_dir) / newspaper_name / category / date_folder
 
@@ -84,7 +138,7 @@ class VNExpressCrawler(BaseCrawler):
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh.connect(ssh_host, username=ssh_user, password=ssh_password)
             sftp = ssh.open_sftp()
-            
+
             # Xử lý URL ảnh
             # Tạo thư mục nếu chưa có (đệ quy)
             path_parts = str(remote_dir).split('/')
@@ -111,20 +165,20 @@ class VNExpressCrawler(BaseCrawler):
                 'file_size': len(image_data.getbuffer())
             }
             save_image_metadata(image_data)
-            
+
             return str(remote_path)
-            
+
         except Exception as e:
             print(f"Lỗi khi tải ảnh {image_url}: {e}")
             return None
 
     def extract_content(self, url: str) -> tuple:
         content = requests.get(url, headers=headers).content
-        sleep_time = random.uniform(1, 3)
+        sleep_time = random.uniform(1, 2)
         time.sleep(sleep_time)
         soup = BeautifulSoup(content, "html.parser")
 
-        title = soup.find("h1", class_="title-detail") 
+        title = soup.find("h1", class_="title-detail")
         if title == None:
             return None, None, None, None, None, None, None, None
         title = title.text
@@ -158,7 +212,7 @@ class VNExpressCrawler(BaseCrawler):
         # Lấy tất cả các ảnh trong nội dung bài viết
         image_tags = soup.find_all("img", class_="lazy") # VNExpress thường dùng class "lazy" cho ảnh trong nội dung
         content_image_urls = [img.get("data-src") for img in image_tags if img.get("data-src")]
-    
+
         return title, description, paragraphs, published_date, image_url, comments, author, content_image_urls
 
     def write_content(self, url: str, article_type: str) -> bool:
@@ -166,10 +220,10 @@ class VNExpressCrawler(BaseCrawler):
             title, description, paragraphs, published_date, image_url, comments, author, content_image_urls = self.extract_content(url)
             if not title:  # Nếu không có tiêu đề, bỏ qua bài viết
                 return None
-            
+
             # Lấy thể loại từ URL
             category = article_type
-                
+
             # Tải và lưu ảnh nội dung
             content_image_paths = []
             for img_url in content_image_urls:
@@ -192,15 +246,15 @@ class VNExpressCrawler(BaseCrawler):
             }
 
             return article_data
-            
+
         except Exception as e:
-            print(f"Lỗi khi xử lý URL {url}: {e}")       
+            print(f"Lỗi khi xử lý URL {url}: {e}")
             return None
 
     def get_urls_of_type_thread(self, article_type, page_number):
         page_url = f"https://vnexpress.net/{article_type}-p{page_number}"
         content = requests.get(page_url, headers=headers).content
-        sleep_time = random.uniform(1, 3)
+        sleep_time = random.uniform(1, 2)
         time.sleep(sleep_time)
         soup = BeautifulSoup(content, "html.parser")
         titles = soup.find_all(class_="title-news")
@@ -213,16 +267,16 @@ class VNExpressCrawler(BaseCrawler):
         for title in titles:
             link = title.find_all("a")[0]
             articles_urls.append(link.get("href"))
-    
+
         return articles_urls
 
     def get_all_articles(self, max_pages):
         """Lấy tất cả bài báo từ các danh mục trên VNExpress."""
         all_articles = []
-        
+
         for category in self.article_type_dict.values():
             for page in range(1, max_pages + 1):
                 urls = self.get_urls_of_type_thread(category, page)
                 all_articles.extend(urls)
-        
+
         return all_articles
