147a851edee4cf8e6124c8259f88e380175495a4
lantuan2205 <minhhuy22051999@gmail.com>
2025-07-11 07:35:45 +0000

    feat: update domain crawler by keyword

--- app/crawl_request.py
+++ app/crawl_request.py
@@ -68,7 +68,20 @@ def process_crawl(data: dict):
         return {"status": "ok", "url": url, "message": f"Đã crawl {len(urls)} bài viết. Dữ liệu đang được lưu."}
     else:
         keyword = url.strip()
-        domains_to_crawl = ["vietnamnet.vn", "vnexpress.net", "dantri.com.vn"]
+        domains_to_crawl = ["thoibaotaichinhvietnam.vn",
+                            "thanhtra.com.vn",
+                            "www.qdnd.vn",
+                            "qltt.vn",
+                            "baotintuc.vn",
+                            "baovephapluat.vn",
+                            "baodantoc.vn",
+                            "tapchicongthuong.vn",
+                            "www.tainguyenvamoitruong.vn",
+                            "dangcongsan.vn",
+                            "phunumoi.net.vn",
+                            "vneconomy.vn",
+                            "kinhtedouong.vn",
+                            "thuonghieuvaphapluat.vn",]
 
         for domain in domains_to_crawl:
             try:
