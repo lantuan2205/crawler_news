0b48f0f88b53979f95b0f2e346db0da7560d39f8
tuannn <tuan.nn@htigroup.vn>
2025-10-16 08:58:39 +0000

    feat: limit 200 post for news

--- app/crawl_request.py
+++ app/crawl_request.py
@@ -181,10 +181,17 @@ def process_crawl(data: Dict[str, Any]):
                 get_profile_domain(crawler, url, False, proxy_session, jobId, crawlId)
                 return
             # Crawl toàn bộ domain
+            MAX_ARTICLES = 200
             total_articles_crawled = 0
             for category in crawler.article_type_dict.values():
+                if total_articles_crawled >= MAX_ARTICLES:
+                    break
+
                 urls = crawler.get_all_articles(category)
                 for article_url in urls:
+                    if total_articles_crawled >= MAX_ARTICLES:
+                        break
+
                     if article_url:
                         get_article_details(crawler, article_url, False, has_video, proxy_session, jobId, crawlId)
                         if "comment" in data_to_collect:
