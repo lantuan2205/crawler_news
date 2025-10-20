3d6f0155e3f6d9e2418e34cadc4d58e8f29e5b33
tuannn <tuan.nn@htigroup.vn>
2025-10-20 08:38:34 +0000

    feat: update config limit news

--- app/crawl_request.py
+++ app/crawl_request.py
@@ -180,11 +180,11 @@ def process_crawl(data: Dict[str, Any]):
             if "profile" in data_to_collect:
                 get_profile_domain(crawler, url, False, proxy_session, jobId, crawlId)
             # Crawl toàn bộ domain
-            MAX_ARTICLES = 2
+            MAX_ARTICLES = 10
             total_articles_crawled = 0
             for category in crawler.article_type_dict.values():
-                if total_articles_crawled >= MAX_ARTICLES:
-                    break
+                # if total_articles_crawled >= MAX_ARTICLES:
+                #     break
 
                 urls = crawler.get_all_articles(category)
                 for article_url in urls:
@@ -194,7 +194,7 @@ def process_crawl(data: Dict[str, Any]):
                     if article_url:
                         get_article_details(crawler, article_url, False, has_video, proxy_session, jobId, crawlId)
                         if "comment" in data_to_collect:
-                            get_comment_details(crawler, url, False, proxy_session, jobId, crawlId)
+                            get_comment_details(crawler, article_url, False, proxy_session, jobId, crawlId)
                         total_articles_crawled += 1
             if "podcast" in data_to_collect:
                 data = crawler.crawl_postcast()
